8c5d676e3c10b2c9e57f874fef729c7fd42b9089
GijsVerschuren <153927139+GijsVerschuren@users.noreply.github.com>
2025-01-11 20:24:26 +0100

    revert changes
    .....yeah

--- index/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.querySelector('.logo').addEventListener('click', logoClick);
-});
-
-function logoClick() {
-    location.href = '../index/index.html';
-}
