795f8fbd0f82c4a87b37c984efd11ab6252cb78d
GijsVerschuren <153927139+GijsVerschuren@users.noreply.github.com>
2025-01-16 15:16:31 +0100

    icons linked
    the icons in the topbar now also link

--- index/script.js
+++ index/script.js
@@ -1,3 +1,15 @@
 function backToHome() {
     window.location.href = "../index/index.html";
 }
+
+function account() {
+    window.location.href = "../account/account.html";
+}
+
+function favorite() {
+    window.location.href = "../favorieten/favorieten.html";
+}
+
+function winkelwagen() {
+    window.location.href = "../bestelpagina/bestel.html";
+}
